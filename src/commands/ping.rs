cc691beeb4b6c972fe45b57ad6aee0ac5a076a94
WW <ww@wwology.tech>
2025-01-25 05:31:47 +0000

    updated ping to use shard gateway latency instead

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -1,19 +1,22 @@
-use std::time::Instant;
-
-use poise::CreateReply;
-
 use crate::{Context, Error};
 
 #[poise::command(slash_command)]
 pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
-    let start = Instant::now();
-    let msg = ctx.reply("Pinging...").await?;
-    let latency = start.elapsed().as_millis();
-    msg.edit(
-        poise::Context::Application(ctx),
-        CreateReply::default().content(format!("Pong! {latency}ms")),
-    )
-    .await?;
+    let shard_manager = ctx.framework.shard_manager;
+    let latency = shard_manager
+        .runners
+        .lock()
+        .await
+        .get(&ctx.framework.serenity_context.shard_id)
+        .ok_or("Failed to get shard")?
+        .latency;
+
+    if let Some(latency) = latency {
+        ctx.reply(format!("Pong {latency:?}ms")).await?;
+    } else {
+        ctx.reply("Please wait for 1 minute to hear the bot heartbeat ")
+            .await?;
+    }
 
     Ok(())
 }
