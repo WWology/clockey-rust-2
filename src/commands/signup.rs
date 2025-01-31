996e4c0b165350ab1ad23992c28ea65a29530d9e
WW <ww@wwology.tech>
2025-01-31 05:33:01 +0000

    removed constant in signup

--- src/commands/signup.rs
+++ src/commands/signup.rs
@@ -11,7 +11,3 @@ pub use gardener::gardener;
 pub use invoice::invoice;
 pub use manual::manual;
 pub use report::report;
-
-const DOTA_CHANNEL_ID: u64 = 738_009_797_932_351_519;
-const CS_CHANNEL_ID: u64 = 746_618_267_434_614_804;
-const OG_STAGE_CHANNEL_ID: u64 = 1_186_593_338_300_842_025;
