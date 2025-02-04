0f704b10edba061810d88214c41b21d97765885c
WW <ww@wwology.tech>
2025-02-04 18:21:24 +0000

    formatting fix for consistency

--- src/commands/prediction/score_adder.rs
+++ src/commands/prediction/score_adder.rs
@@ -7,16 +7,15 @@ use crate::{data::score, Context, Error};
 pub async fn dotaadd(ctx: Context<'_>, role: Role) -> Result<(), Error> {
     ctx.defer().await?;
 
-    let winners: Vec<u64>;
-    {
+    let winners: Vec<u64> = {
         let guild_members = &ctx.guild().ok_or("Failed to find guild")?.members;
 
-        winners = guild_members
+        guild_members
             .iter()
             .filter(|member| member.1.roles.contains(&role.id))
             .map(|member| member.0.get())
-            .collect();
-    }
+            .collect()
+    };
 
     let winners_count = winners.len();
     for id in winners {
