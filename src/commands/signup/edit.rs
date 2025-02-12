659d1a99ac39179a4f7319220811fa0c3f7379fc
WW <ww@wwology.tech>
2025-02-12 09:17:19 +0000

    Modified reply message to help see old event details and what's been changed

--- src/commands/signup/edit.rs
+++ src/commands/signup/edit.rs
@@ -14,39 +14,43 @@ pub async fn edit(
 ) -> Result<(), Error> {
     let mut msg = ctx.http().get_message(ctx.channel_id(), message_id).await?;
     let mut new_message = msg.content.clone();
-
+    let mut reply_message = String::from("Updated event details: \n");
     if let Some(new_name) = name {
-        let name_range = Regex::new(r"-\s*(.*?)\s*,")?
+        let name_match = Regex::new(r"-\s*(.*?)\s*,")?
             .captures(new_message.as_str())
             .ok_or("Failed to find name")?
             .get(1)
-            .ok_or("Failed to find name")?
-            .range();
+            .ok_or("Failed to find name")?;
+        reply_message.push_str(format!("{} to {new_name}\n", name_match.as_str()).as_str());
+        let name_range = name_match.range();
         new_message.replace_range(name_range, new_name.as_str());
     }
 
     if let Some(new_time) = time {
-        let time_range = Regex::new(r"<t:(\d+):F>")?
+        let time_match = Regex::new(r"<t:(\d+):F>")?
             .captures(new_message.as_str())
             .ok_or("Failed to find time")?
             .get(1)
-            .ok_or("Failed to find time")?
-            .range();
+            .ok_or("Failed to find time")?;
+        reply_message.push_str(format!("{} to {new_time}\n", time_match.as_str()).as_str());
+        let time_range = time_match.range();
         new_message.replace_range(time_range, new_time.as_str());
     }
 
     if let Some(new_hours) = hours {
-        let hours_range = Regex::new(r"\badd\s*(\d)\s*")?
+        let hours_match = Regex::new(r"\badd\s*(\d)\s*")?
             .captures(new_message.as_str())
             .ok_or("Failed to find hours")?
             .get(1)
-            .ok_or("Failed to find hours")?
-            .range();
+            .ok_or("Failed to find hours")?;
+        reply_message.push_str(format!("{} to {new_hours}\n", hours_match.as_str()).as_str());
+        let hours_range = hours_match.range();
         new_message.replace_range(hours_range, new_hours.as_str());
     }
 
     msg.edit(&ctx, EditMessage::new().content(new_message.as_str()))
         .await?;
-    ctx.reply("Updated the signup message").await?;
+
+    ctx.reply(reply_message).await?;
     Ok(())
 }
