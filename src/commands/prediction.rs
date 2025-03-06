7f561b1049ebf3c8e00b848ee699ac637bd19111
WW <ww@wwology.tech>
2025-03-06 12:30:25 +0000

    even more rivals functionality

--- src/commands/prediction.rs
+++ src/commands/prediction.rs
@@ -3,7 +3,9 @@ mod score_adder;
 mod show;
 mod winners;
 
-pub use best_of::{csbo, deletecs, deletedota, deleteextra, dotabo, extrabo};
-pub use score_adder::{csadd, dotaadd};
+pub use best_of::{
+    csbo, deletecs, deletedota, deleteextra, deleterivals, dotabo, extrabo, rivalsbo,
+};
+pub use score_adder::{csadd, dotaadd, rivalsadd};
 pub use show::show;
 pub use winners::winners;
